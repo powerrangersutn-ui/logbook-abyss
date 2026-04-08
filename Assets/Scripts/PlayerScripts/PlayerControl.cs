1d60d3e9c28c28a3d1c403a63e7d940517b46de7
Tahiel Ludi <luditahiel@gmail.com>
2026-04-08 02:05:32 -0300

    Cambiando el player controller, sin el nuevo input system, en aarmas

--- Assets/Scripts/PlayerScripts/PlayerControl.cs
+++ Assets/Scripts/PlayerScripts/PlayerControl.cs
@@ -1,70 +1,151 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerControl : MonoBehaviour
 {
-    
-    private Rigidbody rb;
     [Header("Movement")]
-    [SerializeField] private float mvSpeed;
+    [SerializeField] private float walkSpeed = 5.0f;
+    [SerializeField] private float sprintMultiplier = 2.0f;
 
-    [Header("Camera")]
-    [SerializeField] private Vector2 sensitivity;
-    [SerializeField] private Transform cameraTransform;
-    private float camUpMax= 80;
-    private float camDownMin = 280;
+    [Header("Fall Parameters")]
+    [SerializeField] private float gravity = 1.0f;
+    //por ahora no hay salto, lo dejo por las dudas
+    //[SerializeField] private float jumpForce = 5.0f;
 
-    void Start()
+    [Header("Look Sesnsitivity")]
+    [SerializeField] private float mouseSensitivity = 2.0f;
+    [SerializeField] private float upDownRange = 80.0f;
+
+    //Esto es para poder cambiar los controles
+    [Header("Inputs Customisation")]
+    [SerializeField] private string horizontalMoveInput = "Horizontal";
+    [SerializeField] private string verticalMoveInput = "Vertical";
+    [SerializeField] private string mouseXInput = "Mouse X";
+    [SerializeField] private string mouseYInput = "Mouse Y";
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    //por ahora no hay salto, lo dejo por las dudas
+    //[SerializeField] private KeyCode jumpKey = KeyCode.Space;
+
+    //Poner sonidos de pasos nunca fué tam difi-, digo, fácil :D
+    [Header("Footstep Sounds")]
+    [SerializeField] private AudioSource footsteepSource;
+    [SerializeField] private AudioClip[] footsteepSounds;
+    [SerializeField] private float walkStepInterval = 1f;
+    [SerializeField] private float sprintStepInterval = 0.5f;
+    [SerializeField] private float velocityTreshold = 2.0f;
+
+    private int lastPlayedIndex = -1;
+    private bool isMoving;
+    private float nextStepTime;
+    private Camera mainCamera;
+    private float verticalRotation;
+    private Vector3 currentMovement = Vector3.zero;
+    private CharacterController characterController;
+
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+        mainCamera = Camera.main;
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        UpdateMovement();
-        UpdateMouseLook();
+        HandleMovement();
+        HandleRotation();
+        HandleFootstep();
     }
 
-    //movimiento wasd
-    private void UpdateMovement()
+    void HandleMovement()
     {
-        float hor = Input.GetAxisRaw("Horizontal");
-        float ver = Input.GetAxisRaw("Vertical");
-        Vector3 mVelocity = Vector3.zero;
-        if (hor != 0 || ver != 0)
-        {
-            Vector3 direction = (transform.forward * ver + transform.right * hor).normalized;
-            mVelocity = direction * mvSpeed;
-        }
-        mVelocity.y = rb.linearVelocity.y;
-        rb.linearVelocity = mVelocity;
+        float verticalInput = Input.GetAxis(verticalMoveInput);
+        float horizontalInput = Input.GetAxis(horizontalMoveInput);
+
+        float speedMultiplier = Input.GetKey(sprintKey) ? sprintMultiplier : 1f; //Uso de sugar code, si apreto la tecla, speedMultiplier vale sprintMultiplier, si no vale 1
+
+        //float verticalSpeed = verticalInput * walkSpeed * speedMultiplier;
+        //float horizontalSpeed = horizontalInput * walkSpeed * speedMultiplier;
+
+        Vector3 horizontalMovement = new Vector3(horizontalInput, 0, verticalInput);
+        horizontalMovement.Normalize();
+        horizontalMovement = transform.rotation * horizontalMovement * walkSpeed * speedMultiplier;
+
+        HandleGravity();
+
+        currentMovement.x = horizontalMovement.x;
+        currentMovement.z = horizontalMovement.z;
+
+        characterController.Move(currentMovement * Time.deltaTime);
+
+        isMoving = verticalInput != 0 || horizontalInput != 0;
     }
 
-    //Seguir movimiento del mouse
-    private void UpdateMouseLook()
+    void HandleGravity()
     {
-        float hor = Input.GetAxis("Mouse X");
-        float ver = Input.GetAxis("Mouse Y");
-        if (hor != 0)
-        {
-            transform.Rotate( 0, hor * sensitivity.x, 0);
-        }
+        if (characterController.isGrounded)
+            currentMovement.y = -0.5f;
+        else
+            currentMovement.y -= gravity * Time.deltaTime;
+    }
 
-        if (ver != 0)
+
+    //por si agregamos un salto
+    /*void HandleGravityAndJumping()
+    {
+        if (characterController.isGrounded)
         {
-            Vector3 rotation = cameraTransform.localEulerAngles;
-            rotation.x = (rotation.x - ver * sensitivity.y + 360) % 360;
-            //Limitar la cámara en su eje vertical
-            if(rotation.x>camUpMax && rotation.x < 180)
+            currentMovement.y = -0.5f;
+            if (Input.GetKeyDown(jumpKey))
             {
-                rotation.x = camUpMax;
-            }else if(rotation.x < camDownMin && rotation.x > 180)
-            {
-                rotation.x = camDownMin;
+                currentMovement.y = jumpForce;
             }
-            cameraTransform.localEulerAngles = rotation;
+        }
+        else
+        {
+            currentMovement.y-= gravity * Time.deltaTime;
+        }
+    }*/
+
+    void HandleRotation()
+    {
+        float mouseXRotation = Input.GetAxis(mouseXInput) * mouseSensitivity;
+        transform.Rotate(0, mouseXRotation, 0);
+
+        verticalRotation -= Input.GetAxis(mouseYInput) * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
+
+    void HandleFootstep()
+    {
+        float currentStepInterval = (Input.GetKey(sprintKey) ? sprintStepInterval : walkStepInterval);
+        if (characterController.isGrounded && isMoving && Time.time > nextStepTime && characterController.velocity.magnitude > velocityTreshold)
+        {
+            PlayFootstepSounds();
+            nextStepTime = Time.time + currentStepInterval;
+        }
+    }
+
+    void PlayFootstepSounds()
+    {
+        int randomIndex; //por si tenemos varios sonidos de pasos
+        if (footsteepSounds.Length == 1)
+        {
+            randomIndex = 0;
+        }
+        else
+        {
+            randomIndex = (int)Random.Range(0, footsteepSounds.Length - 1);
+            if (randomIndex >= lastPlayedIndex)
+            {
+                randomIndex++;
+            }
         }
 
+        lastPlayedIndex = randomIndex;
+        footsteepSource.clip = footsteepSounds[randomIndex];
+        footsteepSource.Play();
     }
 }
