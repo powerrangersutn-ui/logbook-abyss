eca6d514c676da7626af2d1511995d4c98867d32
Tahiel Ludi <luditahiel@gmail.com>
2026-04-18 17:12:19 -0300

    sirena no sigue al jugador cuando está cerca, arreglado

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -39,16 +39,18 @@ public class EnemyMovementTowardsPlayer : MonoBehaviour
     void Update()
     {
         if (player == null) return;
-
+        
         float distance = Vector3.Distance(transform.position, player.position);
 
         // Si esta cerca
         if (distance < maxDistance)
         {
+            GetComponent<EnemyPatrol>().playerDetected = true;
             // Si esta muy cerca, atacar
             if (distance <= minDistance)
             {
                 // Mirar al player
+                
                 transform.LookAt(player.position);
 
                 // Intentar atacar si paso el cooldown
@@ -63,7 +65,9 @@ public class EnemyMovementTowardsPlayer : MonoBehaviour
                 transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
                 transform.LookAt(player.position);
             }
+            return;
         }
+        GetComponent<EnemyPatrol>().playerDetected = false;
     }
 
     private void Attack()
