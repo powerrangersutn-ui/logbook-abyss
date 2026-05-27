716ad2717c7f6c9f230efe16e6acf4bb7e720187
Tahiel1 <luditahiel@gmail.com>
2026-05-27 10:45:02 -0300

    añandiendo sonido al lobby y cambiando sonido del switch

--- Assets/Scripts/Sound/AudioManager.cs
+++ Assets/Scripts/Sound/AudioManager.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    private bool isMuted = false;
     public void ToggleMuteWithPause()
     {
         // true silences all audio, false resumes it
